af0e9c89817e07d66b98a9df375d78268530ab30
breakthatbass <gamache.taylor@gmail.com>
2021-04-09 11:15:33 -0400

    reorganize and add the level ability

--- main.c
+++ main.c
@@ -9,40 +9,62 @@ int main(int argc, char **argv)
 	char session_id[SESSION];
     char *html_response;
 
+    //char *a = debug_html("tests/rightanswer.txt");
+    //char *b = debug_html("tests/wronganswer.txt");
+
+    //printf("%s\n", b);
+
+    //free(b);
+
+    //exit(0);
+
     // year and day is required, these vars keep track
     int d = 0;
     int y = 0;
     int submit = 0;
     int input = 0;
+    //int part = 0;
+    //char level = 0;
     //int puzzle = 0;
 
     char *year = NULL;
     char *day = NULL;
     char *answer = NULL;
+    char level[2] = "1"; //part 1 by default
 
      // options on left side of ':' take args, on right, no args
-    while ((opt = getopt(argc, argv, "y:d:s:ip")) != -1) {
+    while ((opt = getopt(argc, argv, "y:d:s:l:ip")) != -1) {
         switch(opt) {
         // need args
-        case 'y':
+        case 'y':   // required
             year = optarg;
             y = 1;
             // if arg isnt a number, quit
             if (!isdigit(optarg[0])) print_usage();
             //printf("YEAR: %s\n", year);
             break;
-        case 'd':
+        case 'd':   // required
             day = optarg;
             d = 1;
             // if arg isnt a number, quit
             if (!isdigit(optarg[0])) print_usage();
             //printf("DAY: %s\n", optarg);
             break;
-        // no args
         case 's':
             // submit an answer
             answer = optarg;
             submit = 1;
+            break;
+        case 'l':
+            // l for level, part 1 or 2 for submitting
+            if (strcmp(optarg, "2") == 0) {
+                level[0] = 0;
+                level[1] = 0;
+                strcpy(level, optarg);
+            } else if (strcmp(optarg, "1")) {
+                fprintf(stderr, "-l flag only accepts 1 or 2 as arguments\n");
+                exit(EXIT_FAILURE);
+            }
             break;
         case 'i':
             // get input for puzzle
@@ -61,22 +83,38 @@ int main(int argc, char **argv)
     // make sure a year and day has been given
     if (!d || !y) print_usage();
 
-    // cant get input and submit
-    if (submit && input) {
-        print_usage();
-    } else if (submit && !input) {
-        char header[45] = "level=1&answer=";
+    if (submit && !input) {
+        // heaser to send the part and answer along with the POST
+        char header[BUF] = "level=";
+        strcat(header, level);
+        strcat(header, "&answer=");
         strcat(header, answer);
+
         url = concat_url(year, day, 's');
         strcpy(session_id, get_session_id(getenv("HOME")));
+
+        // send the answer and get back the HTML
         html_response = submit_answer(url, session_id, header);
-        
-        //printf("%s\n", html_response);
-        
+
+        // parse the HTML just to say whether we passed or failed
+        char *results = parse_results(html_response);
+        free(html_response);
+        if (results) {
+            printf("%s\n", results);
+            free(results);
+        } else {
+            // there will probbaly always be a response of some sort but just in case...
+            fprintf(stderr, "problem fetching response\n");
+            exit(EXIT_FAILURE);
+        }
+
     } else if ((!submit && input) || (!submit && !input)) {
         url = concat_url(year, day, 'i');
         strcpy(session_id, get_session_id(getenv("HOME")));
         get_input(url, session_id);
+    } else {
+        //something went wrong somewhere
+        print_usage();
     }
    
     free(url);
