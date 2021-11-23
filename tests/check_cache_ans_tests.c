77c157833a338feaff7bb1d0bf0496db5553aca5
breakthatbass <gamache.taylor@gmail.com>
2021-11-23 11:15:48 -0500

    remove old tests from old program, update test runner

---
+++ tests/check_cache_ans_tests.c
@@ -0,0 +1,46 @@
+#include "minunit/minunit.h"
+#include "../src/eggnog.h"
+#include <stdlib.h>
+
+static char *s1;
+static char *s2;
+static char *s3;
+static char *s4;
+
+void test_setup(void)
+{
+    // exists
+    s1 = check_cache_answers("2020", "1", "r");
+    // doesn't exist
+    s2 = check_cache_answers("2020", "2", "r");
+    // exists
+    s3 = check_cache_answers("2020", "3", "w");
+    // deosn't exist
+    s4 = check_cache_answers("2020", "4", "w");
+}
+
+void test_teardown(void)
+{
+    free(s1);
+    free(s3);
+}
+
+MU_TEST(test_string_eq){
+	mu_assert_string_eq("1234\n", s1);
+    mu_assert_string_eq(NULL, s2);
+	mu_assert_string_eq("345\n", s3);
+	mu_assert_string_eq(NULL, s4);
+}
+
+MU_TEST_SUITE(test_suite)
+{
+    MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
+
+    MU_RUN_TEST(test_string_eq);
+}
+
+int main() {
+	MU_RUN_SUITE(test_suite);
+	MU_REPORT();
+	return MU_EXIT_CODE;
+}
